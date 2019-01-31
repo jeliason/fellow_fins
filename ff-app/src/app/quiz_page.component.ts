6a6296e0b4ba106a067cd9558508ee14e721845b
Jordan Buchanan <jordanbuchanan@JBUCHANAN-MBP-HOU.local>
2019-01-31 14:44:06 -0600

    added some thing to quiz

--- ff-app/src/app/quiz_page.component.ts
+++ ff-app/src/app/quiz_page.component.ts
@@ -1,2 +1,17 @@
-var personCode = Math.floor(Math.random() * 53);
-console.log(personCode);
+import { Component} from '@angular/core';
+import {PEOPLE} from './ff';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz_page.component.html',
+  styleUrls: ['./quiz_page.component.css']
+})
+export class QuizPageComponent{
+  title = 'ff-app';
+  people = PEOPLE;
+
+  selectRandomPerson (){
+    var randomPerson = Math.floor(Math.random() * 53);
+    console.log(randomPerson);
+  }
+}
