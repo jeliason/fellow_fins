941906ea6c5d3c6b155d41013880983da4d2fcc3
Joel Eliason <joel.eliason@parivedasolutions.com>
2019-01-31 16:10:18 -0600

    showFront

--- ff-app/src/app/app.component.ts
+++ ff-app/src/app/app.component.ts
@@ -9,4 +9,21 @@ import {PEOPLE} from './ff';
 export class AppComponent{
   title = 'ff-app';
   people = PEOPLE;
+  showFront = true;
+
+  counter = 0;
+  score = 0;
+
+  full_array=Array.from({length:this.people.length},(x,i)=>i);
+  select_index = this.full_array;
+
+  initQuiz() {
+    this.counter = 0;
+    this.score = 0;
+    this.select_index = this.full_array;
+    this.showFront=false;
+  }
+  hideQuiz(){
+    
+  }
 }
