a39b37506b1e17d332da2e9485e08c5f7b05e094
Jordan Buchanan <jordanbuchanan@JBUCHANAN-MBP-HOU.local>
2019-01-31 16:21:38 -0600

    made the quiz ts work

--- ff-app/src/app/quiz_page.component.ts
+++ ff-app/src/app/quiz_page.component.ts
@@ -11,10 +11,17 @@ export class QuizPageComponent{
   people = PEOPLE;
 
   getFin (){
-    var correctFinCode = Math.floor(Math.random() * 53);
+    var arr = []
+    while(arr.length < 54){
+        var correctFinCode = Math.floor(Math.random()*53);
+        if(arr.indexOf(correctFinCode) === -1) arr.push(correctFinCode);
+    }
     console.log(correctFinCode);
 
     var correctFinName = PEOPLE[correctFinCode].Name;
+    console.log(correctFinName);
+    
     var correctFinPic = PEOPLE[correctFinCode].Picture_Local;
+    console.log(correctFinPic);
   }
 }
